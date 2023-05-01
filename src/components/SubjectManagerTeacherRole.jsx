991ff2edf932334df478d96b15d0933c2ec29254
Nguyen Dang Hoang Thi <thinguyen280601@gmail.com>
2023-05-01 16:09:37 +0700

    update user role subject manager

--- src/components/SubjectManagerTeacherRole.jsx
+++ src/components/SubjectManagerTeacherRole.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
-import TeacherCreateSubject from "./TeacherCreateSubject";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import TeacherCreateSubject from "./TeacherCreateSubject";
 
 import {
-  faCoffee,
   faBell,
+  faCoffee,
   faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
 import classNames from "classnames";
+import { API_ROUTES, axiosInstance } from "../cons";
 import TeacherViewSubject from "./TeacherViewSubject";
 
 const icons = [faCoffee, faBell, faEnvelope];
 const SubjectManagerTeacherRole = () => {
   const [teacherCreateSJModal, setTeacherCreateSJModal] = useState(false);
   const [teacherViewSJModal, setTeacherViewSJModal] = useState(false);
+  const [sj, setSj] = useState();
   const handleCloseModal = () => {
     setTeacherCreateSJModal(false);
     setTeacherViewSJModal(false);
@@ -22,8 +24,47 @@ const SubjectManagerTeacherRole = () => {
   const icon = icons[1];
   const badgeNumber = 2;
   const hasBadge = badgeNumber > 0;
+  // const [selectedSubject, setSelectedSubject] = useState(null);
+
+
+  const getSubjectHandler = (sj) => {
+    // setSelectedSubject(sj.name);
+
+    axiosInstance
+      .get(API_ROUTES.getSubject + `?id=${sj}`, {
+        headers: {
+          Authorization: "bearer " + sessionStorage.getItem("token"),
+        },
+      })
+      .then((data) => setSj(data.data.result))
+      .catch((err) => console.log(err));
+  };
+
 
   const iconClassNames = classNames("text-gray-400", "text-xl", "mr-2");
+
+  const [subjects, setSubjects] = useState([]);
+  useEffect(() => {
+    axiosInstance
+      .get(API_ROUTES.getSubjects, {
+        headers: {
+          Authorization: "bearer " + sessionStorage.getItem("token"),
+        },
+      })
+      .then((data) => {
+        // let lst = [];
+
+        // for (let item of data.data) {
+        //   if (!item.requestUserMail) {
+        //     lst.push(item);
+        //   }
+        // }
+        console.log("Data", data.data);
+        setSubjects(data.data);
+        console.log(typeof subjects);
+        // console.log("1",subjects)
+      });
+  }, []);
   return (
     <div className="w-full h-[600px]  flex   p-2 space-y-8">
       <div className="fixed flex items-center mr-2 right-4 hover:cursor-pointer">
@@ -60,16 +101,27 @@ const SubjectManagerTeacherRole = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr
-                  className="flex hover:bg-gray-100 hover:cursor-pointer"
-                  onClick={() => setTeacherViewSJModal(true)}
-                >
-                  <td className="w-32 border border-gray-700">Mã môn học</td>
-                  <td className="border border-gray-700 w-72">Tên môn học</td>
-                  <td className="flex-1 border border-gray-700">
-                    Tổng tín chỉ
-                  </td>
-                </tr>
+                {subjects.map((subject) => {
+                  return (
+                    <tr
+                      className="flex hover:bg-gray-100 hover:cursor-pointer"
+                      onClick={() => {
+                        getSubjectHandler(subject.id);
+                        setTeacherViewSJModal(true);
+                      }}
+                    >
+                      <td className="w-32 border border-gray-700">
+                        {subject.id}
+                      </td>
+                      <td className="border border-gray-700 w-72">
+                        {subject.name}
+                      </td>
+                      <td className="flex-1 border border-gray-700">
+                        {subject.totalCredits}
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
@@ -105,6 +157,7 @@ const SubjectManagerTeacherRole = () => {
         <TeacherViewSubject
           visible={teacherViewSJModal}
           onClose={handleCloseModal}
+          sj={sj}
         />
       </div>
     </div>
